064113aee138a73ae892a4de826051289d51db93
David Walters <soaringdesigns@gmail.com>
2015-08-18 20:50:20 +0000

    Update deleteRecord.php

--- deleteRecord.php
+++ deleteRecord.php
@@ -1,18 +1,4 @@
 <?php
-
-//Class: <CMIS485 6380 Web Database Development (2152) >
-//Student Name: <David Walters>
-//Instructor: <Dr. Alla Webb>
-//Assignment #: Project 1
-//Description: <RRE Database and PHP interactivity. >
-//Due Date :<1/25/2015>
-//I pledge that I have completed the programming assignment independently.
-//I have not copied the code from a student or any source.
-//I have not given my code to any student.
-//Sign here: <David Walters>
-//Additional Comments: This script uses odbc connectivity to delete a record.
-
-
     //establish database connection
     include 'databaseConnection.php';
     $dbConnection = DatabaseConnection::connect();
